1340d06065edf6ab1ce0310c5cd040059dfab289
ibnesaad-69 <ibnesaad28@gmail.com>
2025-11-05 23:56:33 +0600

    Add files via upload

--- scripts/cmds/prefix.js
+++ scripts/cmds/prefix.js
@@ -3,12 +3,13 @@ const { utils } = global;
 
 module.exports = {
 	config: {
-		name: "prefix",
-		version: "1.4",
+		name: "prefix2",
+		version: "1.3",
 		author: "NTKhang",
 		countDown: 5,
-		role: 0,
-		description: "Thay đổi dấu lệnh của bot trong box chat của bạn hoặc cả hệ thống bot (chỉ admin bot)",
+		role: 2,
+		shortDescription: "Thay đổi prefix của bot",
+		longDescription: "Thay đổi dấu lệnh của bot trong box chat của bạn hoặc cả hệ thống bot (chỉ admin bot)",
 		category: "config",
 		guide: {
 			vi: "   {pn} <new prefix>: thay đổi prefix mới trong box chat của bạn"
@@ -45,7 +46,7 @@ module.exports = {
 			confirmThisThread: "Please react to this message to confirm change prefix in your box chat",
 			successGlobal: "Changed prefix of system bot to: %1",
 			successThisThread: "Changed prefix in your box chat to: %1",
-			myPrefix: "Hey senpai! ~_~\n🌐 Global prefix: %1\n🛸 Your group chat prefix: %2"
+			myPrefix: "🌐 System prefix: %1\n🛸 Your box chat prefix: %2"
 		}
 	},
 
@@ -95,7 +96,7 @@ module.exports = {
 	},
 
 	onChat: async function ({ event, message, getLang }) {
-		if (event.body && event.body.toLowerCase() === "prefix")
+		if (event.body && event.body.toLowerCase() === "prefix2")
 			return () => {
 				return message.reply(getLang("myPrefix", global.GoatBot.config.prefix, utils.getPrefix(event.threadID)));
 			};
